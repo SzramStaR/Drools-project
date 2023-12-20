02092fd8d628f7af9c6e74e413355ff5f810facb
Miłosz <101997323+SzramStaR@users.noreply.github.com>
2023-12-20 20:44:43 +0100

    Final

--- Projekt/src/main/java/com/sample/CareerDecision.java
+++ Projekt/src/main/java/com/sample/CareerDecision.java
@@ -5,11 +5,12 @@ import org.kie.api.KieServices;
 
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-
 import javax.swing.JOptionPane;
-
+import javax.swing.*;
+import java.awt.*;
 
 public class CareerDecision {
+
 	
 	public static final void main(String[] args) {
 		try {
@@ -17,7 +18,7 @@ public class CareerDecision {
 	        KieServices ks = KieServices.Factory.get();
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
-
+        	
         	// go !
             kSession.fireAllRules();
 		} catch(Throwable t) {
@@ -26,35 +27,7 @@ public class CareerDecision {
 		
 	}
 
-//	public static class Question{
-//		public String question;
-//		public String answer;
-//		public String[] choice;
-//		
-//		public Question(String q, String[] ch) {
-//			this.question = q;
-//			this.choice = ch;
-//			this.answer = null;
-//			
-//		}
-//		
-//		public void showQuestion() {
-//			int pom = JOptionPane.showOptionDialog(null, question, "Please pick your answer" ,  JOptionPane.DEFAULT_OPTION,
-//	        	    JOptionPane.QUESTION_MESSAGE, null, choice, choice[0]);
-//			answer = choice[pom];
-//		
-//		}
-//	
-//	}
-//	  public static class Option {
-//	    	public String option;
-//	    	
-//	    	public Option(String o) {
-//	    		this.option = o;
-//	    		JOptionPane.showMessageDialog(null, o);
-//	    	}
-//	    }
-	
 	
 		
 }
+
